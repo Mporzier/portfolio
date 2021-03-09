eb0f09e4a19dee9a5334a2dbfb1e9888484f104f
Mporzier <mathias.porzier@hotmail.com>
2021-03-09 13:59:12 -0800

    Save progress, publishing into gh pages working

--- src/Components/About.js
+++ src/Components/About.js
@@ -45,7 +45,7 @@ class About extends Component {
               <div className="columns download">
                 <p>
                   <a href={resumeDownload} className="button" target="_blank">
-                    <i className="fa fa-download"></i>Download Resume
+                    <i className="fa fa-eye"></i>View Resume
                   </a>
                 </p>
               </div>
