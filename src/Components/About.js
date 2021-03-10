67a71e66986a4738f39334d3348c07b3b6083589
Mporzier <mathias.porzier@hotmail.com>
2021-03-09 16:07:43 -0800

    Fix header displays, change header background

--- src/Components/About.js
+++ src/Components/About.js
@@ -27,21 +27,8 @@ class About extends Component {
           </div>
           <div className="nine columns main-col">
             <h2>About Me</h2>
-
             <p>{bio}</p>
             <div className="row">
-              <div className="columns contact-details">
-                <h2>Contact Details</h2>
-                <p className="address">
-                  <span>{name}</span>
-                  <br />
-                  {city}, {state}
-                  <br />
-                  <span>{phone}</span>
-                  <br />
-                  <span>{email}</span>
-                </p>
-              </div>
               <div className="columns download">
                 <p>
                   <a href={resumeDownload} className="button" target="_blank">
